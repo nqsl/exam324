881069a5c84096e56c5dea805f06754e8b418669
Brilda <131636604+nqsl@users.noreply.github.com>
2023-04-24 10:02:45 -0400

    Add files via upload

---
+++ cabecera.inc.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejemplo Maquetado</title>
+	<link rel="stylesheet" type="text/css" href="./estilo.css">
+</head>
+<body>
+	<div class="cabecera">
+		<h1 style="font-size: 12px;font-family: arial"><center>Universidad Mayor de San Andres</center></h1><br>
+		<h2 style="font-size: 12px; font-family: arial"><center>Facultad de Ciencias Puras y Naturales</center></h2>
+	</div>
+	      <!-- end loader -->
+      <!-- header -->
